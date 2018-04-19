a5af55e088d5615801f15bb6d80d06a9e874a296
Alisher Koryakovtsev <alisherk@nvidia.com>
2018-04-19 14:44:06 +0300

    Revert install as user and no SSL check

--- .travis/install.sh
+++ .travis/install.sh
@@ -26,6 +26,6 @@ if [[ "$(uname -s)" == 'Linux' ]]; then
     CXX=$CXX_COMPILER
 fi
 
-pip install --user conan
+sudo pip install conan==1.2.0
 
 conan user
--- .travis/run.sh
+++ .travis/run.sh
@@ -16,7 +16,7 @@ fi
 
 mkdir build || true;
 cd build
-conan remote add conan.io https://server.conan.io || true
+conan remote add conan.io https://server.conan.io False || true
 conan install .. -s build_type=$BUILD_TYPE --build=missing
 cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER
 cmake --build . -- VERBOSE=1
