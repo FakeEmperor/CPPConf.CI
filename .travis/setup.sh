efbfc23d8b083f60d350859e855f444dc7e69eea
Alisher Koryakovtsev <alisherk@nvidia.com>
2018-04-19 13:33:49 +0300

    Updated setup script and bumped conan

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -14,7 +14,9 @@ if [[ "$(uname -s)" == 'Linux' ]]; then
     CXX=$CXX_COMPILER
 fi
 
-mkdir build && cd build
+mkdir build || true;
+cd build
+conan remote add conan.io https://server.conan.io || true
 conan install .. -s build_type=$BUILD_TYPE --build=missing
 cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER
 cmake --build . -- VERBOSE=1
